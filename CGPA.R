3dba2009240a270d15ac2533cd81f29cf84c2e3e
Ruben <ruben.larrazolo@gmail.com>
2021-11-14 12:55:59 -0800

    Plots
    Testing Assumptions

--- CGPA.R
+++ CGPA.R
@@ -7,3 +7,14 @@
 ### Normality - Need to examine both GPA and TOEFL score
 
 plotNormalHistogram(graduate_admissions$CGPA)
+
+graduate_admissions$CGPAsq <- graduate_admissions$CGPA * graduate_admissions$CGPA
+plotNormalHistogram(graduate_admissions$CGPAsq)
+
+plotNormalHistogram(graduate_admissions$`TOEFL Score`)
+spec(graduate_admissions)
+
+graduate_admissions$`TOEFL.ScoreSQ` <- graduate_admissions$`TOEFL Score`* graduate_admissions$`TOEFL Score`
+plotNormalHistogram(graduate_admissions$`TOEFL.ScoreSQ`)
+
+
