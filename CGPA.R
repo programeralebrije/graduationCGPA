b28b83515f1cf9c9c62feb9762caad47ffef5646
Ruben <ruben.larrazolo@gmail.com>
2021-11-14 12:46:55 -0800

    Plot
    InitialPlot

---
+++ CGPA.R
@@ -0,0 +1,9 @@
+## https://github.com/programeralebrije/graduationCGPA.git
+
+#This is to predict college GPA holding TOEFL score constant and using university rating as a predictor.
+
+## Testing Assumptions
+
+### Normality - Need to examine both GPA and TOEFL score
+
+plotNormalHistogram(graduate_admissions$CGPA)
